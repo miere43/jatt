16a55975fc29afef2dc008f3e8f6e397b3490af1
miere43 <x.miere@gmail.com>
2017-05-14 12:11:58 +0300

    Use 'do { ... } while (0)' loop instead of brackets in error macros, improve error messages

--- src/TimeTracker/error_macros.h
+++ src/TimeTracker/error_macros.h
@@ -20,28 +20,29 @@ void _callErrorListeners(const char* function, const char* file, int line, const
 
 /** Just like Q_ASSERT, but returns if 'cond' evaluates to false and reports error to registered error handlers. **/
 #define ERR_VERIFY(cond) \
-    { \
+    do { \
         if (!(cond)) { \
-            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition " #cond " failed."); \
+            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition \"" #cond "\" is false."); \
             return; \
         } \
-    }
+    } while (0)
 
 /** Just like Q_ASSERT, but returns specified value if 'cond' evaluates to false and reports error to registered error handlers. **/
 #define ERR_VERIFY_V(cond, returnValue) \
-    { \
+    do { \
         if (!(cond)) { \
-            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition " #cond " failed"); \
+            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition \"" #cond "\" is false, returning \"" #returnValue "\"."); \
             return (returnValue); \
         } \
-    }
+    } while (0)
 
 /** Just like Q_ASSERT, but executes 'continue' statement if 'cond' evaluates to false and reports error to registered error handlers. **/
 #define ERR_VERIFY_CONTINUE(cond) \
-    { \
+    do { \
         if (!(cond)) { \
-            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition " #cond " failed."); \
+            _callErrorListeners(__FUNCTION__, __FILE__, __LINE__, "Condition \"" #cond "\" is false."); \
             continue;         \
         } \
-    }
+    } while (0)
+
 #endif // ERROR_MACROS_H
